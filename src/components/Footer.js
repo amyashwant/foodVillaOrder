2770ebef016b7f948be18107e5dbde949a85d71a
amyashwant <poonamgangwar083@gmail.com>
2023-08-31 23:10:48 +0530

    31ss

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <div className="footer text-center text-sm font-medium font-poppins h-10 flex justify-center items-center bg-slate-900 text-white">
-      <h1 data-testid="footer">© 2023 made by Yashwant 🎉</h1>
-    </div>
-  );
-};
-
-export default Footer;
